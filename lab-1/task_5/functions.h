d80757e3f0b5031437953683553c2917ba7b5962
Nifacy <mai.grishin.alexey@gmail.com>
2024-03-15 03:36:54 +0300

    split task 5 code on internal and external part

--- lab-1/task_5/functions.h
+++ lab-1/task_5/functions.h
@@ -1,6 +1,8 @@
-#include "matrix.h"
-#include <iostream>
-#include <cmath>
+#ifndef _TASK_5_FUNCTIONS_
+#define _TASK_5_FUNCTIONS_
+
+#include "../matrix.h"
+#include <vector>
 #include <complex>
 
 
@@ -126,11 +128,11 @@ void UpdateChangeHistory(const Matrix::TMatrix& A, std::vector<ChangeHistory>& h
 }
 
 
-std::vector<std::complex<float>> GetEigenValues(const Matrix::TMatrix& A, float eps) {
+EigenValues GetEigenValues(const Matrix::TMatrix& A, float eps) {
     int n = std::get<0>(A.GetSize());
     Matrix::TMatrix Q(n, n), R(n, n);
     Matrix::TMatrix Ai = A;
-    std::vector<std::complex<float>> values;
+    EigenValues values;
     std::vector<std::vector<float>> history(n);
     int i = 0;
 
@@ -158,61 +160,4 @@ std::vector<std::complex<float>> GetEigenValues(const Matrix::TMatrix& A, float 
 }
 
 
-int ReadMatrixSize() {
-    int n;
-    std::cout << "Enter matrix size: ";
-    std::cin >> n;
-
-    if (n <= 0) {
-        throw std::runtime_error("matrix size can't be negative or zero");
-    }
-
-    return n;
-}
-
-
-Matrix::TMatrix ReadMatrix() {
-    int n = ReadMatrixSize();
-    Matrix::TMatrix A(n, n);
-    std::cout << "Enter matrix A:" << std::endl;
-    Matrix::Read(A);
-    return A;
-}
-
-
-float ReadEpsilon() {
-    float eps;
-    std::cout << "Enter precision: ";
-    std::cin >> eps;
-
-    if (eps < 0.0) {
-        throw std::runtime_error("precision can't be a negative value");
-    }
-
-    return eps;
-}
-
-
-void PrintEigenValues(const EigenValues& values) {
-    for (const std::complex<float>& value : values) {
-        if (value.imag() == 0.0) {
-            std::cout << value.real() << std::endl;
-        } else {
-            std::cout << value.real();
-            std::cout << ((value.imag() >= 0.0) ? " + " : " - ");
-            std::cout << std::abs(value.imag()) << "i" << std::endl;
-        }
-    }
-}
-
-
-int main(void) {
-    Matrix::TMatrix A = ReadMatrix();
-    float eps = ReadEpsilon();
-    EigenValues values = GetEigenValues(A, eps);
-
-    std::cout << "Eigen values:" << std::endl;
-    PrintEigenValues(values);
-
-    return 0;
-}
+#endif // _TASK_5_FUNCTIONS_
