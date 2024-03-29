dc4a27c6ef8d7cb6ad789d2687a8d600a14dc1c4
Nifacy <mai.grishin.alexey@gmail.com>
2024-03-29 03:20:08 +0300

    make some functions runtime executable for task 1

--- task_1.py
+++ task_1.py
@@ -1,7 +1,27 @@
-from math import log
+from numpy import arange
 
 
-def iterations_method(x0, phi, q, eps, iterations):
+def derivative(f):
+    DX = 0.00001
+    return lambda x: (f(x + DX) - f(x)) / DX
+
+
+def sign(x):
+    if x == 0.0: return 0.0
+    if x < 0.0: return -1.0
+    return 1.0
+
+
+def iterations_method(f, a, b, eps, iterations):
+    x0 = (a + b) / 2.0
+    step = 0.001
+    df = derivative(f)
+    max_df = max(abs(df(x)) for x in arange(a, b, step))
+    sign_f = sign(df(a))
+    phi = lambda x: x - (sign_f / max_df) * f(x)
+    dphi = derivative(phi)
+    q = max(abs(dphi(x)) for x in arange(a, b, step))
+
     last_x = x0
     i = 0
 
@@ -11,11 +31,14 @@ def iterations_method(x0, phi, q, eps, iterations):
             return x
         last_x = x
         i += 1
+
     return last_x
 
 
-def newton_method(x0, f, df, eps, iterations):
+def newton_method(f, a, b, eps, iterations):
+    x0 = (a + b) / 2.0
     last_x = x0
+    df = derivative(f)
     i = 0
 
     while i <= iterations:
@@ -29,9 +52,9 @@ def newton_method(x0, f, df, eps, iterations):
 
 
 f = lambda x: 2 ** x + x ** 2 - 2.0
-df = lambda x: log(2) * (2 ** x) + 2.0 * x
-phi = lambda x: x - (1.0 / df(1.0)) * f(x)
-dphi = lambda x: 1.0 - (1.0 / df(1.0)) * df(x)
+a, b = 0.5, 1.0
+eps = 0.0001
+iterations = 100
 
-print(iterations_method(0.8, phi, dphi(0.0), 0.0001, 100))
-print(newton_method(0.8, f, df, 0.0001, 100))
+print(iterations_method(f, a, b, eps, iterations))
+print(newton_method(f, a, b, eps, iterations))
