efd0dbfa2172eea986fa06739b18acbfd69402b8
Nifacy <mai.grishin.alexey@gmail.com>
2024-03-31 19:59:57 +0300

    add rect selection controller

--- plot_widget/controller.py
+++ plot_widget/controller.py
@@ -1,7 +1,7 @@
 from matplotlib.backend_bases import MouseEvent, MouseButton
 from PyQt5.QtCore import QPointF
 
-from .visualizers import RangeSelectionVisualizer
+from .visualizers import RangeSelectionVisualizer, RectAreaVisualizer
 from .plot_widget import PlotWidget
 
 
@@ -59,6 +59,36 @@ class RangeSelectionController:
         if self._pressed and (event.xdata is not None):
             self._range[1] = event.xdata
             self._visualizer.borders = tuple(sorted(self._range))
+
+    def _on_mouse_release(self, _: MouseEvent) -> None:
+        self._pressed = False
+
+    def _subscribe_on_events(self) -> None:
+        self._widget.mpl_connect("button_press_event", self._on_mouse_press)
+        self._widget.mpl_connect("motion_notify_event", self._on_mouse_move)
+        self._widget.mpl_connect("button_release_event", self._on_mouse_release)
+
+
+class RectSelectionController:
+    def __init__(self, plot_widget: PlotWidget, rect_area: RectAreaVisualizer):
+        self._widget = plot_widget
+        self._rect_area = rect_area
+        self._s1 = (0.0, 0.0)
+        self._s2 = (0.0, 0.0)
+        self._pressed = False
+        self._subscribe_on_events()
+
+    def _on_mouse_press(self, event: MouseEvent) -> None:
+        if event.button == MouseButton.LEFT:
+            self._pressed = True
+            self._s1 = (event.xdata, event.ydata)
+            self._rect_area.coords = (self._s1, self._s1)
+
+    def _on_mouse_move(self, event: MouseEvent) -> None:
+        if self._pressed and (event.xdata is not None):
+            self._s2 = (event.xdata, event.ydata)
+            (x1, x2), (y1, y2) = sorted([self._s1[0], self._s2[0]]), sorted([self._s1[1], self._s2[1]])
+            self._rect_area.coords = ((x1, y1), (x2, y2))
 
     def _on_mouse_release(self, _: MouseEvent) -> None:
         self._pressed = False
