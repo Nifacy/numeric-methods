d337bb80a459df82fa0d3017912e7a0bd9aaed77
Nifacy <mai.grishin.alexey@gmail.com>
2024-04-07 19:38:08 +0300

    move plot widgets in common module

--- lab_2/task_2/app.py
+++ lab_2/task_2/app.py
@@ -1,36 +1,40 @@
-from math import sin
 import sys
-from typing import Any, Callable, Mapping
+from math import *
+from typing import Callable
+
 import numpy as np
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QTimer
-from lab_2.plot_widget.widget import PlotWidget, add_grid
-from lab_2.plot_widget.controllers import RangeSelectionController, RectSelectionController, ResizeController
-from lab_2.plot_widget.widget import PlotUpdateEvent
+from PyQt5.QtWidgets import (
+    QComboBox,
+    QDoubleSpinBox,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QSpinBox,
+    QVBoxLayout,
+    QWidget,
+)
+from sympy import lambdify, symbols, sympify
 
+from common.plot_widget.controllers import RectSelectionController, ResizeController
+from common.plot_widget.objects import Curve, Point, RectArea
+from common.plot_widget.widget import PlotWidget, add_grid
 from lab_2.task_2 import domain
-
-from lab_2.plot_widget.objects import Curve, OneArgFunction, Coords, RangeSelection, RectArea, VerticalLine, Point
-
-from PyQt5.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QComboBox, QDoubleSpinBox, QSpinBox
-
-from math import *
-from sympy import symbols, sympify, lambdify
 
 
 def _expr_to_func(expr: str) -> Callable[[float, float], float]:
-    x_1, x_2 = symbols('x_1 x_2')
-    func = lambdify([x_1, x_2], sympify(expr), 'numpy')
+    x_1, x_2 = symbols("x_1 x_2")
+    func = lambdify([x_1, x_2], sympify(expr), "numpy")
     return func
 
 
 class Task2Window(QWidget):
-    DEFAULT_FUNCTIONS = ('x_1 ** 2 + x_2 ** 2 - 2 ** 2', 'x_1 - exp(x_2) + 2')
+    DEFAULT_FUNCTIONS = ("x_1 ** 2 + x_2 ** 2 - 2 ** 2", "x_1 - exp(x_2) + 2")
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setLayout(self._init_objects())
-
 
     def _init_plot_widget(self):
         self._plot_widget = PlotWidget(self, 20.0)
@@ -38,34 +42,34 @@ class Task2Window(QWidget):
             plot_widget=self._plot_widget,
             f=_expr_to_func(self.DEFAULT_FUNCTIONS[0]),
             styles={
-                'colors': ['red'],
-                'linewidths': [1],
-            }
+                "colors": ["red"],
+                "linewidths": [1],
+            },
         )
         self._func_2_graphic = Curve(
             plot_widget=self._plot_widget,
             f=_expr_to_func(self.DEFAULT_FUNCTIONS[1]),
             styles={
-                'colors': ['blue'],
-                'linewidths': [1],
-            }
+                "colors": ["blue"],
+                "linewidths": [1],
+            },
         )
         self._rect_area = RectArea(
             plot_widget=self._plot_widget,
             s1=(0, 0),
             s2=(0, 0),
             styles={
-                'color': 'blue',
-                'alpha': 0.2,
-                'linewidth': 2,
+                "color": "blue",
+                "alpha": 0.2,
+                "linewidth": 2,
             },
         )
         self._answer_point = Point(
             plot_widget=self._plot_widget,
             pos=(0, 0),
             styles={
-                'color': 'orange',
-                'linewidth': 2,
+                "color": "orange",
+                "linewidth": 2,
             },
         )
 
@@ -77,10 +81,9 @@ class Task2Window(QWidget):
 
         return self._plot_widget
 
-
     def _init_function_1_input(self):
         self._function_1_layout = QHBoxLayout()
-        self._function_1_prefix = QLabel('f_1(x_1, x_2) = ')
+        self._function_1_prefix = QLabel("f_1(x_1, x_2) = ")
         self._function_1_editor = QLineEdit(self)
 
         self._function_1_editor.setText(self.DEFAULT_FUNCTIONS[0])
@@ -90,10 +93,9 @@ class Task2Window(QWidget):
 
         return self._function_1_layout
 
-
     def _init_function_2_input(self):
         self._function_2_layout = QHBoxLayout()
-        self._function_2_prefix = QLabel('f_2(x_1, x_2) = ')
+        self._function_2_prefix = QLabel("f_2(x_1, x_2) = ")
         self._function_2_editor = QLineEdit(self)
 
         self._function_2_editor.setText(self.DEFAULT_FUNCTIONS[1])
@@ -103,31 +105,28 @@ class Task2Window(QWidget):
 
         return self._function_2_layout
 
-
     def _init_method_input(self):
         self._method_layout = QHBoxLayout()
-        self._method_preifx = QLabel('Метод: ')
+        self._method_preifx = QLabel("Метод: ")
         self._method_combo_box = QComboBox()
-        
-        self._method_combo_box.addItems(['Итераций', 'Ньютона'])
-        
+
+        self._method_combo_box.addItems(["Итераций", "Ньютона"])
+
         self._method_layout.addWidget(self._method_preifx)
         self._method_layout.addWidget(self._method_combo_box, 1)
-        
+
         return self._method_layout
 
-
     def _init_error_label(self):
-        self._error_label = QLabel('Ошибка: Функция введена неверно')
-        self._error_label.setStyleSheet('color: red')
+        self._error_label = QLabel("Ошибка: Функция введена неверно")
+        self._error_label.setStyleSheet("color: red")
         self._error_label.hide()
 
         return self._error_label
 
-
     def _init_epsilon_input(self):
         self._epsilon_layout = QHBoxLayout()
-        self._epsilon_label = QLabel('eps = ')
+        self._epsilon_label = QLabel("eps = ")
         self._epsilon_input = QDoubleSpinBox()
 
         self._epsilon_input.setDecimals(10)
@@ -140,10 +139,9 @@ class Task2Window(QWidget):
 
         return self._epsilon_layout
 
-
     def _init_iteration_input(self):
         self._iteration_layout = QHBoxLayout()
-        self._iteration_label = QLabel('Кол-во итераций: ')
+        self._iteration_label = QLabel("Кол-во итераций: ")
         self._iteration_input = QSpinBox()
 
         self._iteration_input.setMinimum(1)
@@ -153,11 +151,10 @@ class Task2Window(QWidget):
 
         return self._iteration_layout
 
-
     def _init_answer_layout(self):
         self._answer_layout = QVBoxLayout()
-        self._answer_label = QLabel('Ответ: x = (0.0000, 0.0000)')
-        self._answer_iterations_label = QLabel('Кол-во итераций: 0')
+        self._answer_label = QLabel("Ответ: x = (0.0000, 0.0000)")
+        self._answer_iterations_label = QLabel("Кол-во итераций: 0")
 
         self._answer_label.hide()
         self._answer_iterations_label.hide()
@@ -167,11 +164,9 @@ class Task2Window(QWidget):
 
         return self._answer_layout
 
-
     def _update_range_label(self):
         (x1, y1), (x2, y2) = self._rect_area.coords
-        self._range_label.setText(f'Область: [{x1:.5f}, {x2:.5f}] x [{y1:.5f}, {y2:.5f}]')
-
+        self._range_label.setText(f"Область: [{x1:.5f}, {x2:.5f}] x [{y1:.5f}, {y2:.5f}]")
 
     def _update_function(self, editor, visualizer):
         def _handler():
@@ -183,13 +178,13 @@ class Task2Window(QWidget):
                 self._error_label.show()
             else:
                 visualizer.function = new_function
-        return _handler
 
+        return _handler
 
     def _run_solution(self):
         choosed_method = self._method_combo_box.currentText()
 
-        if choosed_method == 'Итераций':
+        if choosed_method == "Итераций":
             method = domain.iteration_method
         else:
             method = domain.newton_method
@@ -199,24 +194,25 @@ class Task2Window(QWidget):
         iterations = self._iteration_input.value()
         f1 = self._func_1_graphic.function
         f2 = self._func_2_graphic.function
-        f = domain.VectorFunction([
-            lambda x: f1(x[0], x[1]),
-            lambda x: f2(x[0], x[1]),
-        ])
+        f = domain.VectorFunction(
+            [
+                lambda x: f1(x[0], x[1]),
+                lambda x: f2(x[0], x[1]),
+            ]
+        )
 
         ans, iterations = method(f, s1, s2, eps, iterations)
 
-        self._answer_label.setText(f'Ответ: x = ({ans[0]:.5f}, {ans[1]:.5f})')
-        self._answer_iterations_label.setText(f'Кол-во итераций: {iterations}')
+        self._answer_label.setText(f"Ответ: x = ({ans[0]:.5f}, {ans[1]:.5f})")
+        self._answer_iterations_label.setText(f"Кол-во итераций: {iterations}")
         self._answer_label.show()
         self._answer_iterations_label.show()
         self._answer_point.position = (ans[0], ans[1])
         self._answer_point.show()
 
-
     def _init_objects(self):
-        self._start_button = QPushButton('Start')
-        self._range_label = QLabel('Область: [0, 0] x [0, 0]')
+        self._start_button = QPushButton("Start")
+        self._range_label = QLabel("Область: [0, 0] x [0, 0]")
 
         self._main_layout = QVBoxLayout()
         self._main_layout.addWidget(self._init_plot_widget(), 1)
@@ -232,11 +228,15 @@ class Task2Window(QWidget):
 
         # bindings
 
-        self._plot_widget.mpl_connect('draw_event', lambda _: self._update_range_label())
+        self._plot_widget.mpl_connect("draw_event", lambda _: self._update_range_label())
         self._function_1_editor.textChanged.connect(self._error_label.hide)
         self._function_2_editor.textChanged.connect(self._error_label.hide)
-        self._function_1_editor.editingFinished.connect(self._update_function(self._function_1_editor, self._func_1_graphic))
-        self._function_2_editor.editingFinished.connect(self._update_function(self._function_2_editor, self._func_2_graphic))
+        self._function_1_editor.editingFinished.connect(
+            self._update_function(self._function_1_editor, self._func_1_graphic)
+        )
+        self._function_2_editor.editingFinished.connect(
+            self._update_function(self._function_2_editor, self._func_2_graphic)
+        )
         self._start_button.clicked.connect(self._run_solution)
 
         return self._main_layout
