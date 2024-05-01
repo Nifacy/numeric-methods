84631735296b69dd541f4b3674bbbcaa84026507
Nifacy <mai.grishin.alexey@gmail.com>
2024-05-02 02:45:34 +0300

    add 2nd version of method 2 in task 4-2

--- lab_4/task_2.py
+++ lab_4/task_2.py
@@ -1,6 +1,7 @@
 from .task_1 import runge_kutta
 from lab_2.task_1.domain import newton_method
 from lab_1.task_2 import solve_system
+from lab_1.task_1 import solve_system, lu_decompose
 import numpy as np
 from math import exp
 import matplotlib.pyplot as plt
@@ -35,7 +36,6 @@ def shooting_method(
     nu = found_root(g, 0.0, 4.0, 0.001, 10).x
     Y_0 = build_condition(nu)
     return underlying_method(F, Y_0, X)
-
 
 
 def method_2(p, q, f, cond_1, cond_2, a, b, h):
@@ -94,6 +94,56 @@ def method_2(p, q, f, cond_1, cond_2, a, b, h):
     return answer
 
 
+def solve(A, B):
+    l, u, p = lu_decompose(A)
+    return solve_system(l, u, p, B)
+
+
+def method_2_2(p, q, f, cond_1, cond_2, a, b, h):
+    X = np.arange(a, b + h, h)
+    N = len(X) - 1
+
+    g_1 = lambda x: 1 - 0.5 * p(x) * h
+    g_2 = lambda x: q(x) * h ** 2 - 2
+    g_3 = lambda x: 1 + 0.5 * p(x) * h
+    g_4 = lambda x: f(x) * h ** 2
+
+    A = np.zeros((N + 1, N + 1))
+    B = np.zeros((N + 1, 1))
+
+    a, b, c = cond_1
+    if a == 0:
+        A[0, 0] = 1.0
+        B[0, 0] = -c/b
+
+    elif b == 0:
+        A[0, 0:3] = -3 * a, 4 * a, -a
+        B[0, 0] = - c * 2 * h
+    
+    else:
+        A[0, 0:3] = -3 * a + b * 2 * h, 4 * a, -a
+        B[0, 0] = - c * 2 * h
+
+    a, b, c = cond_2
+    if a == 0:    
+        A[N, -1] = 1.0
+        B[N, 0] = -c/a
+    
+    elif b == 0:
+        A[N, N - 2:N + 1] = a, -4 * a, 3 * a
+        B[N, 0] = -c * 2 * h
+
+    else:
+        A[N, N - 2:N + 1] = a, -4 * a, 3 * a + 2 * b * h
+        B[N, 0] = -c * 2 * h
+
+    for i in range(1, N):
+        A[i, i - 1:i + 2] = g_1(X[i]), g_2(X[i]), g_3(X[i])
+        B[i, 0] = g_4(X[i])
+
+    return np.array(solve(A, B).flat)
+
+
 if __name__ == "__main__":
     p = lambda x: 4 * x / (2 * x + 1)
     q = lambda x: -4 / (2 * x + 1)
@@ -109,7 +159,7 @@ if __name__ == "__main__":
     X = np.arange(0.0, 1.01, 0.1)
 
     result = shooting_method(F, [1.0, 0.0, 1.0], [1.0, 2.0, -3.0], X)
-    result_2 = method_2(
+    result_2 = method_2_2(
         p, q, f,
         (1.0, 0.0, 1.0), (1.0, 2.0, -3.0),
         0.0, 1.0, 0.1,
